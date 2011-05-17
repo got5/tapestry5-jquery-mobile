f50c5ed93283ff0514682ae147650ffed453a2bb
Clement <clement.uster@gmail.com>
2011-05-17 18:28:27 +0200

    just because all roads lead to Seclin...

--- src/test/java/org/got5/tapestry5/jquery/mobile/test/pages/GeolocationPage.java
+++ src/test/java/org/got5/tapestry5/jquery/mobile/test/pages/GeolocationPage.java
@@ -1,6 +1,9 @@
 package org.got5.tapestry5.jquery.mobile.test.pages;
 
 import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.annotations.SetupRender;
+import org.apache.tapestry5.json.JSONLiteral;
 import org.apache.tapestry5.json.JSONObject;
 import org.got5.tapestry5.jquery.mobile.components.Geolocation;
 import org.got5.tapestry5.jquery.mobile.data.GeoPosition;
@@ -13,5 +16,15 @@ public class GeolocationPage {
 		JSONObject o = new JSONObject();
 		o.put("a", "b");
 		return o;
+	}
+	
+	@Property
+	private JSONObject config;
+	
+	@SetupRender
+	void setupRender(){
+		config = new JSONObject();
+		config.put("center", new JSONLiteral("new google.maps.LatLng(50.550851, 3.030454)"));
+		config.put("zoom", 12);
 	}
 }
